a9290b01a77c4fa1a278e20894f6774f53c3d6be
Alec Palmans <alec.palmans@gmail.com>
2016-12-01 22:12:08 +0100

    First Commit

--- Game prins/Program.cs
+++ Game prins/Program.cs
@@ -10,6 +10,41 @@ namespace Game_prins
     {
         static void Main(string[] args)
         {
+            Goblin shmet = new Goblin();
+            shmet.SetStartHealth(120);
+            shmet.Type = types.normal_goblin;
+            Hero Alec = new Hero();
+            Alec.Name = "Alec The Great and Powerfull, he who wields the allmighty sword of anti autism also known as 'Das bruut hé'";
+            Alec.SetStartHealth(120);
+            Console.WriteLine(Alec.Name+" Je start hp is: " + Alec.Health);
+            Alec.SetWeapon(weapons.sword);
+            Hero Othmane = new Hero();
+            Othmane.Name = "Othmane the almighty willy-slayer he who holds the power of the Yasuo-skills also known as 'The immortal Fucker of justice'";
+            int value = (int)Alec.GetWeapon();
+            Console.WriteLine("Je wapen is een " + Alec.GetWeapon());
+            Alec.InLoveWithPrincess = true;
+            if (Othmane.SetStartHealth(110))
+            {
+                Console.WriteLine(Othmane.Name+" Je HP is ingesteld op " + Othmane.Health + ".");
+            }
+            else
+            {
+                Console.WriteLine("Mislukt. HP was al ingesteld");
+            }
+            if (Othmane.SetStartHealth(110))
+            {
+                Console.WriteLine("HP ingesteld op " + Othmane.Health + ".");
+            }
+            else
+            {
+                Console.WriteLine("Mislukt. HP was al ingesteld");
+            }
+            Othmane.SetWeapon(weapons.Katana);
+            Console.WriteLine("Je wapen is een "+Othmane.GetWeapon());
+            Alec.Attack(shmet, 40);
+            Console.WriteLine("Alec valt shmet(de goblin) aan.");
+            Console.WriteLine("De goblin heeft nu nog " + shmet.Health);
+            Console.Read();
         }
     }
 }
