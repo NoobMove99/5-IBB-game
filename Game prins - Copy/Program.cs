f0b2ed3bc0a11cd2679a9909557949858a8d8f6e
NoobMove99 <alec.palmans@gmail.com>
2016-12-08 16:21:37 +0100

    Add files via upload

--- Game prins - Copy/Program.cs
+++ Game prins - Copy/Program.cs
@@ -14,21 +14,21 @@ namespace Game_prins
             Console.BackgroundColor = ConsoleColor.White;
             Console.Clear();
 
-            Hero Alec = new Hero();
-            Alec.SetStartHealth(150);
-            Alec.Name = "Alec";
-            Alec.SetWeapon(weapons.fist);
-            Alec.Money = 0;
+            Hero h1 = new Hero();
+            h1.SetStartHealth(150);
+            h1.Name = "Alec";
+            h1.SetWeapon(weapons.fist);
+            h1.Money = 0;
 
-            Goblin Niels = new Goblin();
-            Niels.SetStartHealth(110);
-            Niels.Name = "Niels";
-            Niels.Type = types.normal_goblin;
+            Goblin g1 = new Goblin();
+            g1.SetStartHealth(110);
+            g1.Name = "Niels";
+            g1.Type = types.normal_goblin;
 
-            Prinses Anneleen = new Prinses();
-            Anneleen.Name = "Anneleen";
-            Anneleen.Leeftijd = 17;
-            Anneleen.HairColor = HairColors.Brunette;
+            Prinses p1 = new Prinses();
+            p1.Name = "Anneleen";
+            p1.Leeftijd = 17;
+            p1.HairColor = HairColors.Brunette;
 
             Beer Bruno = new Beer();
             Bruno.Name = "Bruno";
@@ -44,103 +44,111 @@ namespace Game_prins
             if (start == "ja")
             {
                 Console.WriteLine("Geef de naam in van je held: ");
-                Alec.Name = Console.ReadLine();
-                Console.WriteLine(Alec.Name + " gaat op weg.");
-                Console.WriteLine(Alec.Name + " komt een goblin tegen op de weg. Hoe wil je hem noemen?");
-                Niels.Name = Console.ReadLine();
-                Console.WriteLine("Wilt u "+Niels.Name+" aanvallen?(typ ja of nee)");
+                h1.Name = Console.ReadLine();
+                Console.WriteLine(h1.Name + " gaat op weg.");
+                System.Threading.Thread.Sleep(2000);
+                Console.WriteLine(h1.Name + " komt een goblin tegen op de weg. Hoe wil je hem noemen?");
+                g1.Name = Console.ReadLine();
+                Console.WriteLine("De goblin loopt op je af! Wilt u "+g1.Name+" aanvallen?(typ ja of nee)");
                 string aanval = Console.ReadLine().ToLower();
 
-                if (aanval == "ja" && Niels.Health >= 1)
+                if (aanval == "ja" && g1.Health >= 1)
                 {
-                    Alec.Attack(Niels);
-                    Console.WriteLine(Alec.Name + " valde " + Niels.Name + " aan en nu heeft " + Niels.Name + " nog " + Niels.Health + " HP over.");
+                    h1.Attack(g1);
+                    Console.WriteLine(h1.Name + " valde " + g1.Name + " aan en nu heeft " + g1.Name + " nog " + g1.Health + " HP over.");
                 }
-                else if (aanval == "ja" && Niels.Health == 0)
+                else if (aanval == "ja" && g1.Health == 0)
                 {
-                    Console.WriteLine(Alec.Name + " begroef de goblin " + Niels.Name + " en liet hem zo eindelijk tot rust komen.");
+                    Console.WriteLine(h1.Name + " begroef de goblin " + g1.Name + " en liet hem zo eindelijk tot rust komen.");
                 }
                 else if (aanval == "nee")
                 {
                     Console.WriteLine("Ze staren elkaar aan...");
                 }
                 System.Threading.Thread.Sleep(2000);
-                Console.WriteLine(Niels.Name+" is bang en loopt weg.");
+                Console.WriteLine(g1.Name+" is bang en loopt weg.");
                 System.Threading.Thread.Sleep(1500);
-                Console.WriteLine(Alec.Name + " vervolgd zijn weg");
+                Console.WriteLine(h1.Name + " vervolgd zijn weg");
                 System.Threading.Thread.Sleep(2000);
-                Console.WriteLine(Alec.Name + " komt aan bij een kasteel en klopt op de grote houten poort.");
+                Console.WriteLine(h1.Name + " komt aan bij een kasteel en klopt op de grote houten poort.");
                 System.Threading.Thread.Sleep(2000);
-                Console.WriteLine("De poort gaat open en voor " + Alec.Name + " staat een schone prinses.");
+                Console.WriteLine("De poort gaat open en voor " + h1.Name + " staat een schone prinses.");
                 Console.WriteLine("Hoe heet deze prinses?");
-                Anneleen.Name = Console.ReadLine();
-                Console.WriteLine(Anneleen.Name + " vraagt of je wilt blijven eten.(ja of nee)");
+                p1.Name = Console.ReadLine();
+                System.Threading.Thread.Sleep(2000);
+                Console.WriteLine(p1.Name + " vraagt of je wilt blijven eten.(ja of nee)");
                 string eten = Console.ReadLine();
                 if (eten == "ja")
                 {
-                    Console.WriteLine(Alec.Name + " gaat binnen en krijgt een HP-Boost");
-                    Alec.Health += 50;
-                    Console.WriteLine(Alec.Name + " heeft nu een Hp van: " + Alec.Health);
+                    Console.WriteLine(h1.Name + " gaat binnen en krijgt een HP-Boost");
+                    h1.Health += 50;
+                    Console.WriteLine(h1.Name + " heeft nu een Hp van: " + h1.Health);
+                    System.Threading.Thread.Sleep(2000);
+                    Console.WriteLine(h1.Name + " neemt afscheid van " + h1.Name + " en vervolgt zijn weg.");  
                 }
                 else if (eten == "nee")
                 {
-                    Console.WriteLine(Alec.Name + " verlaat het kasteel.");
+                    Console.WriteLine(h1.Name + " verlaat het kasteel.");
                 }
-                Console.WriteLine(Alec.Name + " komt aan een kruispunt en hij heeft drie opties(rechtdoor,links,rechts).");
+                System.Threading.Thread.Sleep(3000);
+                Console.WriteLine(h1.Name + " komt aan een kruispunt en hij heeft drie opties(rechtdoor,links,rechts).");
                 System.Threading.Thread.Sleep(2000);
                 Console.WriteLine("Welke weg wil je inslaan?");
                 string weg = Console.ReadLine();
                 if (weg == "rechtdoor")
                 {
-                    Console.WriteLine(Alec.Name + " stapt rechtdoor.");
-                    Console.WriteLine(Alec.Name + " komt in een bos en ziet in de verte een hutje staan.");
+                    Console.WriteLine(h1.Name + " stapt rechtdoor.");
+                    System.Threading.Thread.Sleep(2000);
+                    Console.WriteLine(h1.Name + " komt in een bos en ziet in de verte een hutje staan.");
                     Console.WriteLine("Wilt u binnengaan?(ja of nee)");
                     string enter = Console.ReadLine();
                     if (enter == "ja")
                     {
-                        Console.WriteLine(Alec.Name + " gaat binnen en ziet een oude man in een stoel zitten.");
+                        Console.WriteLine(h1.Name + " gaat binnen en ziet een oude man in een stoel zitten.");
+                        System.Threading.Thread.Sleep(1000);
                         Console.WriteLine("De oude man begint te vertellen...");
-                        System.Threading.Thread.Sleep(3000);
+                        System.Threading.Thread.Sleep(2000);
                         Console.WriteLine("Othmane's lore");
                     }
                     else if (enter == "nee")
                     {
-                        Console.WriteLine(Alec.Name + " stapt langs het huisje en valt in een diepe put.");
-                        Console.WriteLine(Alec.Name + " sterft van de dorst.");
+                        Console.WriteLine(h1.Name + " stapt langs het huisje en valt in een diepe put.");
+                        System.Threading.Thread.Sleep(5000);
+                        Console.WriteLine(h1.Name + " sterft van de dorst.");
                         System.Threading.Thread.Sleep(2500);
                         Environment.Exit(0);
                     }
                 }
                 else if (weg == "links")
                 {
-                    Console.WriteLine(Alec.Name + " gaat naar links.");
+                    Console.WriteLine(h1.Name + " gaat naar links.");
                     Console.WriteLine("Hij komt aan een groot meer.");
                     Console.WriteLine("Wil je erdoor zwemmen of eromheen gaan?(z of e)");
                     string meer = Console.ReadLine().Substring(0, 1).ToUpper();
                     if (meer == "Z")
                     {
-                        Console.WriteLine(Alec.Name + " komt veilig aan de overkant.");
+                        Console.WriteLine(h1.Name + " komt veilig aan de overkant.");
                     }
                     else if (meer == "E")
                     {
                         Console.WriteLine("Op de weg staat er een beer van " + Bruno.grootte + " en met een HP van " + Bruno.Health);
                         System.Threading.Thread.Sleep(1500);
-                        Console.WriteLine(Alec.Name + " wordt aangevallen door de beer.");
+                        Console.WriteLine(h1.Name + " wordt aangevallen door de beer.");
                         Console.WriteLine("Wilt u aanvallen?(ja of nee)");
                         string beer = Console.ReadLine();
                         if (beer == "ja")
                         {
-                            Alec.AttackBeer(Bruno);
-                            Console.WriteLine(Alec.Name + " valt de beer aan.");
+                            h1.AttackBeer(Bruno);
+                            Console.WriteLine(h1.Name + " valt de beer aan.");
                             System.Threading.Thread.Sleep(2500);
                             Console.WriteLine("De beer heeft nu nog " + Bruno.Health + " HP over.");
                             Console.WriteLine("De beer is geschrokken van jouw moed en loopt weg.");
-                            Console.WriteLine(Alec.Name + " rust even uit en stapt dan weer verder.");
+                            Console.WriteLine(h1.Name + " rust even uit en stapt dan weer verder.");
 
                         }
                         else if (beer == "nee")
                         {
-                            Console.WriteLine(Alec.Name + " wordt opgegeten door de beer.");
+                            Console.WriteLine(h1.Name + " wordt opgegeten door de beer.");
                             System.Threading.Thread.Sleep(3000);
                             Environment.Exit(0);
                         }
@@ -148,33 +156,36 @@ namespace Game_prins
                 }
                 else if (weg == "rechts")
                 {
-                    Console.WriteLine(Alec.Name + " gaat naar rechts.");
-                    Console.WriteLine(Alec.Name + " ziet een chest liggen.");
+                    Console.WriteLine(h1.Name + " gaat naar rechts.");
+                    Console.WriteLine(h1.Name + " ziet een chest liggen.");
                     Console.WriteLine("Wil je deze chest openmaken?(ja of nee)");
                     string chest = Console.ReadLine();
                     if (chest == "ja")
                     {
-                        Console.WriteLine(Alec.Name + " vindt 500 euro.");
-                        Alec.Money += 500;
+                        Console.WriteLine(h1.Name + " vindt 500 euro.");
+                        h1.Money += 500;
                     }
                     else if (chest == "nee")
                     {
-                        Console.WriteLine(Alec.Name + " vervolgt zijn weg.");
+                        Console.WriteLine(h1.Name + " vervolgt zijn weg.");
                     }
                 }
+                System.Threading.Thread.Sleep(5000);
                 Console.WriteLine("Je komt aan een klein stadje met een paar huizen en een paar winkeltjes.");
+                System.Threading.Thread.Sleep(2000);
                 Console.WriteLine("Wat wil je eerst doen?(supermarkt of wapenwinkel)");
                 string winkel = Console.ReadLine();
                 if (winkel=="supermarkt")
                 {
-                    Console.WriteLine("Je koopt wat eten.");
+                    Console.WriteLine("Je koopt wat ravioli.");
+                    System.Threading.Thread.Sleep(2000);
                     Console.WriteLine("Wil je het nu opeten?");
                     string supermarkt = Console.ReadLine();
                     if (supermarkt=="ja")
                     {
-                        Console.WriteLine(Alec.Name + " krijgt een HP-boost");
-                        Alec.Health += 50;
-                        Console.WriteLine(Alec.Name + " zijn HP is nu " + Alec.Health + " HP.");
+                        Console.WriteLine(h1.Name + " krijgt een HP-boost");
+                        h1.Health += 50;
+                        Console.WriteLine(h1.Name + " zijn HP is nu " + h1.Health + " HP.");
                     }
                     else if (supermarkt=="nee")
                     {
@@ -188,7 +199,7 @@ namespace Game_prins
                     string nieuwwapen = Console.ReadLine();
                     Console.WriteLine("Je hebt nu een " + nieuwwapen + ".");
                 }
-                Console.WriteLine(Alec.Name + " ziet een hotel staan. Wil je er overnachten?");
+                Console.WriteLine(h1.Name + " ziet een hotel staan. Wil je er overnachten?");
                 string hotel = Console.ReadLine();
                 if (hotel=="ja")
                 {
@@ -196,18 +207,29 @@ namespace Game_prins
                     string kamer = Console.ReadLine();
                     if (kamer=="ja")
                     {
-                        Console.WriteLine(Alec.Name + " betaalt 50 euro en gaat op zijn bed liggen.");
-                        Alec.Money -= 50;
+                        Console.WriteLine(h1.Name + " betaalt 50 euro en gaat op zijn bed liggen.");
+                        h1.Money -= 50;
                     }
                     else if (kamer=="nee")
                     {
-                        Console.WriteLine(Alec.Name + " verlaat het hotel en gaat terug op weg...");
-                        Console.WriteLine(Alec.Name + " valt van de vermoeidheid op de grond in slaap.");
+                        Console.WriteLine(h1.Name + " verlaat het hotel en gaat terug op weg...");
+                        System.Threading.Thread.Sleep(2000);
+                        Console.WriteLine(h1.Name + " valt van de vermoeidheid op de grond in slaap.");
+                        System.Threading.Thread.Sleep(2000);
                         Console.WriteLine("'s nachts wordt hij overvallen en hij is 100 euro kwijt.");
-                        Alec.Money -= 100;
-                        Console.WriteLine(Alec.Name + " heeft nu nog " + Alec.Money + " over.");
+                        h1.Money -= 100;
+                        Console.WriteLine(h1.Name + " heeft nu nog " + h1.Money + " over.");
                     }
-                    
+                    Console.WriteLine(h1.Name + " stapt voor twee dagen aan één stuk en komt aan een splitsing.");
+                    System.Threading.Thread.Sleep(1000);
+                    Console.WriteLine("Welke weg wil je nemen(rechts of links)?");
+                    string splitsing = Console.ReadLine();
+                    if (splitsing=="rechts")
+                    {
+                        Console.WriteLine(h1.Name + " neemt het rechtse pad.");
+                        System.Threading.Thread.Sleep(1000);
+                        Console.WriteLine("lemao");
+                    }
                 }
             }
             else
@@ -221,8 +243,9 @@ namespace Game_prins
                 else
                 {
                     Console.WriteLine("U done fucked up.");
+                    System.Threading.Thread.Sleep(1000);
                     Console.WriteLine("System shutdown in 3,2,1,...");
-                    System.Threading.Thread.Sleep(5000);
+                    System.Threading.Thread.Sleep(3000);
                     Environment.Exit(0);
                 }
             }
